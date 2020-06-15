cb0dd27590d2cb0d6557bb0a8b68ccfe97901383
komal-habura <64227198+komal-habura@users.noreply.github.com>
2020-06-15 15:33:50 +0530

    Add files via upload

---
+++ key & securitygroup.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  region     = "ap-south-1"
+  profile    = "mycloud2"
+}
+
+
+//create key
+resource "aws_key_pair" "myterrakey" {
+  key_name   = "myterrakey"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 komalhabura2@gmail.com"
+}
+
+output "myout1" {
+  value = "aws_key_pair.private_key
+}
+//create security group
+
+resource "aws_security_group" "mysecurity1" {
+  name        = "mysecurity1"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-6fb9a407"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mysecurity1"
+  }
+}
+
